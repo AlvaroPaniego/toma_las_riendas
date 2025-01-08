c810693392c1e0571d4a20379656ef4d3f8d9a85
Alvaro <alvaro.paniego.fernandez@gmail.com>
2025-01-08 11:32:50 +0100

    arreglado lo de las imagenes del carrousel

--- src/components/shared/CarouselItem.vue
+++ src/components/shared/CarouselItem.vue
@@ -1,7 +1,8 @@
 <template>
     <Transition name="slide-in" mode="ease-in-out">
         <div class="carousel-item" v-show="currentSlide === index">
-            <img :src= "slide"/>
+            <!-- <img :src="slide" alt=""> -->
+            <img :src="require(`@/assets/${slide}`)" alt="">
         </div>
     </Transition>
 </template>
@@ -21,9 +22,11 @@ export default {
     right: 0;
     left: 0;
 }
-.slide-in-enter-from{
+
+.slide-in-enter-from {
     transform: translateX(100%);
 }
+
 .slide-in-leave-to {
     transform: translateX(-100%);
 }
